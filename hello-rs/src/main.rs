deda3471eaa0ae478de4fd1dd7523ab1fdf57f19
omar ma <mapinxue@qq.com>
2024-11-25 19:41:30 +0800

    init rust

---
+++ hello-rs/src/main.rs
@@ -0,0 +1,17 @@
+#[macro_use] extern crate rocket;
+
+use rocket::fs::FileServer;
+
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/public", FileServer::from("static"))
+
+}
