a662dc05f6ff954cff60cb87a6135fcf22f8b346
omar ma <mapinxue@qq.com>
2024-11-27 19:18:23 +0800

    add components

--- freighter/src/main.tsx
+++ freighter/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import LoginPage from './pages/user/login/index.tsx'
+
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter >
+      <Routes>
+        <Route index element={<App />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ freighter/src/pages/user/login/index.tsx
@@ -0,0 +1,13 @@
+import Login from "@/components/Login"
+
+/**
+ * 页面布局：
+ * * 期望： https://pro.magicui.design/login
+ * 1. 使用 Card 组件作为基座
+ * 2. 使用表单提供登陆
+ */
+export default function LoginPage () {
+    return (
+        <Login />
+    )
+}
