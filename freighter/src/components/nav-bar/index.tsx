8581cbd8a571989d840d93f37c6e8206d3a805b4
omar ma <mapinxue@qq.com>
2024-12-18 11:35:45 +0800

    header

--- freighter/src/components/nav-bar/index.tsx
+++ freighter/src/components/nav-bar/index.tsx
@@ -1,18 +1,28 @@
 import { Button } from "@/components/ui/button"
+import { Link } from "react-router"
+import InteractiveHoverButton from "../ui/interactive-hover-button"
+import { GithubIcon } from "../icons"
 
 export default function Home() {
   return (
-    <div className="flex justify-between px-12 py-4 sticky top-0 backdrop-blur-2xl  border-b border-slate-900/10">
+    <div className="px-8 py-4 flex justify-between sticky top-0 backdrop-blur-2xl ">
       <div className="flex">
-        <div className="h-6">
-          <img src="/logo.png" className="size-6 inline-block m-1" />
-          <span className="inline-block relative top-[3px] font-bold">资产管理系统</span>
+        <div>
+          <img src="/logo.png" className="size-10 inline-block" />
+          <span className="inline-block font-bold ml-2 relative top-[4px] text-lg">资产管理系统</span>
         </div>
-        <div className="px-8">nav1</div>
-        <div className="px-8">nav2</div>
-        <div className="px-8">nav3</div>
+
       </div>
-      <div>option</div>
+      <div className="flex">
+        <Link className="relative top-[9px] mr-8" to="/">文档</Link>
+        <Link className="relative top-[9px] mr-8" to="/login">关于</Link>
+
+        <Button variant="ghost" size="icon" className="mr-8">
+          <GithubIcon />
+        </Button>
+
+        <InteractiveHoverButton text="演示" className="h-10"></InteractiveHoverButton>
+      </div>
     </div>
   )
 }
